8ce1d843dda5ec65ad61ce2f0c35fbc1505ccd0b
hipoloco <rodrigofernandezfranco@gmail.com>
2020-05-25 15:12:07 -0300

    export.js improvements
    - Check if recording URL is set
    - Check if recording URL has the correct format
    - Use meeting ID as export name if it isn't defined
    - Check if duration is a natural number
    - Check if convert is set in true or false
    - Use real recording duration if "duration" isn't set or if it's set to 0
    - Catch URL unreachable error

--- export.js
+++ export.js
@@ -43,15 +43,42 @@ async function main() {
         if(platform == "linux"){
             xvfb.startSync()
         }
-        var url = process.argv[2],
-            exportname = process.argv[3], 
-            duration = process.argv[4],
-            convert = process.argv[5]
+        
+        var url = process.argv[2];
+        if(!url){
+            console.warn('URL undefined!');
+            process.exit(1);
+        }
+        // Verify if recording URL has the correct format
+        var urlRegex = new RegExp('^https?:\\/\\/.*\\/playback\\/presentation\\/2\\.0\\/playback\\.html\\?meetingId=[a-z0-9]{40}-[0-9]{13}');
+        if(!urlRegex.test(url)){
+            console.warn('Invalid recording URL!');
+            process.exit(1);
+        }
 
-        if(!url){ url = 'https://www.mynaparrot.com/' }
-        if(!exportname){ exportname = 'export.webm' }
-        if(!duration){ duration = 10 }
-        if(!convert){ convert = false }
+        var exportname = process.argv[3];
+        // Use meeting ID as export name if it isn't defined.
+        if(!exportname){
+            exportname = url.split("=")[1] + '.webm';
+        }
+        
+        var duration = process.argv[4];
+        // If duration isn't defined, set it in 0
+        if(!duration){
+            duration = 0;
+        // Check if duration is a natural number
+        } else if(!Number.isInteger(duration) || duration < 0){
+            console.warn('Duration must be a natural number!');
+            process.exit(1);
+        }
+        
+        var convert = process.argv[5]
+        if(!convert){
+            convert = false
+        } else if(convert !== true || convert !== false){
+            console.warn("Invalid convert value!");
+            process.exit(1);
+        }
         
         const browser = await puppeteer.launch(options)
         const pages = await browser.pages()
@@ -64,9 +91,22 @@ async function main() {
         });
 
         await page._client.send('Emulation.clearDeviceMetricsOverride')
-        await page.goto(url, {waitUntil: 'networkidle2'})
+        // Catch URL unreachable error
+        await page.goto(url, {waitUntil: 'networkidle2'}).catch(e => {
+            console.error('Recording URL unreachable!');
+            process.exit(2);
+        })
         await page.setBypassCSP(true)
 
+        // Get recording duration
+        var recDuration = await page.evaluate(() => {
+            return document.getElementById("video").duration;
+        });
+        // If duration was set to 0 or is greater than recDuration, use recDuration value
+        if(duration == 0 || duration > recDuration){
+            duration = recDuration;
+        }
+        
         await page.waitForSelector('button[class=acorn-play-button]');
         await page.$eval('#navbar', element => element.style.display = "none");
         await page.$eval('#copyright', element => element.style.display = "none");
@@ -184,4 +224,3 @@ function copyOnly(filename){
         console.log(err)
     }
 }
-
